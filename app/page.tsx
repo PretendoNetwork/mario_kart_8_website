4be060a37caabb1ecf09ded314d9ce26102d0862
Rambo6Glaz <39063367+EpicUsername12@users.noreply.github.com>
2024-04-21 16:42:16 +0200

    Announce incoming maintenance changelog and allow admin page (maintenance/kickall) to specific PID

--- app/page.tsx
+++ app/page.tsx
@@ -6,199 +6,164 @@ import { useEffect, useState } from "react";
 import { Alert, Badge, Carousel, ListGroup, Tab, Tabs } from "react-bootstrap";
 
 const HomePage = () => {
-    function getTimeLeftUntilEnd(): string | null {
-        const currentDate = new Date();
-        const currentUTCDate = new Date(currentDate.getTime() + currentDate.getTimezoneOffset() * 60000);
+	return (
+		<div className="container-fluid mt-5 mb-3 h-100 p-0 d-flex justify-content-center align-items-center">
+			<form className="text-center w-100 p-5 bg-light shadow-lg">
+				<h1 className="mb-3">Welcome to the Pretendo Mario Kart 8 website</h1>
+				<h6>
+					<small className="text-muted">A full game server replacement for MK8</small>
+				</h6>
+				<Alert variant="info" className="w-75 mx-auto">
+					<p>Update changelog:</p>
+					<div>
+						<li>Fix DataStore uploads (this fixes rankings, MKTV uploads)</li>
+					</div>
+				</Alert>
+				<Tabs defaultActiveKey="home" className="mb-3">
+					<Tab eventKey="home" title="Overview">
+						<Carousel className="mb-3" variant="dark">
+							<Carousel.Item>
+								<img
+									src="/session1.png"
+									alt="Lots of nostalgia"
+									className="d-block w-100 mx-auto"
+									style={{ maxHeight: "720px", maxWidth: "1280px" }}
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/compe1.png"
+									alt="Tournament being played"
+									className="d-block w-100 mx-auto"
+									style={{ maxHeight: "720px", maxWidth: "1280px" }}
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/mktv1.png"
+									alt="Tournament being played"
+									className="d-block w-100 mx-auto"
+									style={{ maxHeight: "720px", maxWidth: "1280px" }}
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/compe2.png"
+									alt="Tournament being played"
+									className="d-block w-100 mx-auto"
+									style={{ maxHeight: "720px", maxWidth: "1280px" }}
+								/>
+							</Carousel.Item>
+						</Carousel>
+						<p>{"Astute readers may have noticed how I (Rambo6Glaz) won both tournaments."}</p>
+						<Alert>
+							<Alert.Heading>How to play on this server?</Alert.Heading>
+							<hr />
+							<p>
+								• Follow the steps to join the Pretendo Network on <Link href="https://pretendo.network">our website</Link>!
+							</p>
+							<p>• Create / log on your PNID account and simply start Mario Kart 8</p>
+						</Alert>
+						<Alert variant="success">
+							<Alert.Heading>Is there any rules?</Alert.Heading>
+							<hr />
+							<p>• You may only use cheats in friend rooms</p>
+							<p>• You should not disrupt gameplay or server uptime</p>
+							<p>• {"Do not generate any user-content (tournament, Mii names) with injury or NSFW content."}</p>
+							<p>• You may have fun. {"(If red shells allow you)"}</p>
+							<p>• Have common sense, we may issue a ban even for something that is not on list</p>
+							<hr />
+							<p className="mb-0">
+								<strong>Breaching the rules can get your account or console temporarily/permanently banned from our services</strong>{" "}
+								(🤓)
+							</p>
+						</Alert>
+					</Tab>
+					<Tab eventKey="features" title="Features">
+						<h3>{"What's implemented ?"}</h3>
 
-        // Define the target date (8th April 4 PM PDT)
-        const targetDate = new Date("2024-04-08T16:00:00-07:00");
-        if (currentUTCDate > targetDate) {
-            return null;
-        }
+						<div className={`mx-auto w-auto`}>
+							<ListGroup className="mb-3">
+								<strong>
+									<ListGroup.Item variant="primary">Works for everyone</ListGroup.Item>
+									<ListGroup.Item variant="info">
+										Fully/partially a Miiverse related feature, might no be available on Cemu, might require a Tester account.
+									</ListGroup.Item>
+									<ListGroup.Item variant="danger">Does NOT work</ListGroup.Item>
+								</strong>
+							</ListGroup>
+							<hr />
 
-        const differenceMs = targetDate.getTime() - currentUTCDate.getTime();
-        const hours = Math.floor(differenceMs / (1000 * 60 * 60));
-        const minutes = Math.floor((differenceMs % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((differenceMs % (1000 * 60)) / 1000);
+							<ListGroup className="mt-3 mb-3">
+								<strong>
+									<ListGroup.Item variant="primary">Global matchmaking</ListGroup.Item>
+									<ListGroup.Item variant="primary">Regional matchmaking</ListGroup.Item>
+									<ListGroup.Item variant="primary">Friend rooms</ListGroup.Item>
+									<ListGroup.Item variant="primary">Join a friend by the friend list / friend presence</ListGroup.Item>
+									<ListGroup.Item variant="primary">Join a recent player</ListGroup.Item>
+								</strong>
+							</ListGroup>
+							<hr />
 
-        const formattedTime = `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+							<ListGroup className="mt-3 mb-3">
+								<strong>
+									<ListGroup.Item variant="info">Tournaments creation / deletion / update</ListGroup.Item>
+									<ListGroup.Item variant="primary">Tournament participation</ListGroup.Item>
+									<ListGroup.Item variant="primary">Tournament search by filter or code</ListGroup.Item>
+									<ListGroup.Item variant="primary">Tournament rankings (team and normal)</ListGroup.Item>
+									<ListGroup.Item variant="info">Post in the Tournament community</ListGroup.Item>
+									<ListGroup.Item variant="info">View posts from the tournament community</ListGroup.Item>
+								</strong>
+							</ListGroup>
+							<hr />
 
-        return formattedTime;
-    }
+							<ListGroup className="mt-3 mb-3">
+								<strong>
+									<ListGroup.Item variant="primary">Rankings</ListGroup.Item>
+									<ListGroup.Item variant="primary">Rankings ghost download</ListGroup.Item>
+									<ListGroup.Item variant="info">Rankings Miiverse posts download</ListGroup.Item>
+									<ListGroup.Item variant="info">Rankings ghost upload</ListGroup.Item>
+								</strong>
+							</ListGroup>
+							<hr />
 
-    const [timeLeft, setTimeLeft] = useState(getTimeLeftUntilEnd());
+							<ListGroup className="mt-3 mb-3">
+								<strong>
+									<ListGroup.Item variant="primary">Mario Kart TV</ListGroup.Item>
+									<ListGroup.Item variant="primary">Mario Kart TV global / popular / recent highlights</ListGroup.Item>
+									<ListGroup.Item variant="info">Mario Kart TV highlight download</ListGroup.Item>
+									<ListGroup.Item variant="primary">Mario Kart TV highlight upload</ListGroup.Item>
+									<ListGroup.Item variant="danger">Mario Kart TV highlight upload on YouTube</ListGroup.Item>
+								</strong>
+							</ListGroup>
+							<hr />
+						</div>
+					</Tab>
+					<Tab eventKey="contact" title="CEMU">
+						<Alert variant="info">
+							<p>• Use CEMU 2.0-43 experimental or higher</p>
+							<p>• {"We don't support piracy and we recommend dumping files from your own console."}</p>
+							<p>• {"Files downloaded from the website aren't supported anymore"}</p>
+							<p>
+								•{" "}
+								{
+									"If you have unstable frames, plesase don't go online, it ruins the experience for everyone, if it happens too much you will be banned."
+								}
+							</p>
+						</Alert>
+						<Alert>
+							<p>
+								The server is available on console and CEMU, follow the tutorial on{" "}
+								<Link href="https://pretendo.network">the website</Link> to get Pretendo Network on your device.
+							</p>
+						</Alert>
+					</Tab>
+				</Tabs>
+			</form>
+		</div>
+	);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTimeLeft(getTimeLeftUntilEnd());
-        }, 1000);
-
-        return () => clearInterval(interval);
-    });
-
-    return (
-        <div className="container-fluid mt-5 mb-3 h-100 p-0 d-flex justify-content-center align-items-center">
-            <form className="text-center w-100 p-5 bg-light shadow-lg">
-                <h1 className="mb-3">Welcome to the Pretendo Mario Kart 8 website</h1>
-                <h6>
-                    <small className="text-muted">A full game server replacement for MK8</small>
-                </h6>
-                {
-                    !timeLeft ? (
-                        <Alert variant="primary" className="mt-3">
-                            The official server will be closed in: <strong>{timeLeft}</strong>
-                        </Alert>
-                    ) : (
-                        <Alert variant="danger" className="mt-3">
-                            <strong>The official Nintendo server closed on the 8th of April at 4pm EDT</strong>
-                        </Alert>
-                    )
-                }
-                <Tabs defaultActiveKey="home" className="mb-3">
-                    <Tab eventKey="home" title="Overview">
-                        <Carousel className="mb-3" variant="dark">
-                            <Carousel.Item>
-                                <img
-                                    src="/session1.png"
-                                    alt="Lots of nostalgia"
-                                    className="d-block w-100 mx-auto"
-                                    style={{ maxHeight: "720px", maxWidth: "1280px" }}
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    src="/compe1.png"
-                                    alt="Tournament being played"
-                                    className="d-block w-100 mx-auto"
-                                    style={{ maxHeight: "720px", maxWidth: "1280px" }}
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    src="/mktv1.png"
-                                    alt="Tournament being played"
-                                    className="d-block w-100 mx-auto"
-                                    style={{ maxHeight: "720px", maxWidth: "1280px" }}
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    src="/compe2.png"
-                                    alt="Tournament being played"
-                                    className="d-block w-100 mx-auto"
-                                    style={{ maxHeight: "720px", maxWidth: "1280px" }}
-                                />
-                            </Carousel.Item>
-                        </Carousel>
-                        <p>{"Astute readers may have noticed how I (Rambo6Glaz) won both tournaments."}</p>
-                        <Alert>
-                            <Alert.Heading>How to play on this server?</Alert.Heading>
-                            <hr />
-                            <p>
-                                • Follow the steps to join the Pretendo Network on <Link href="https://pretendo.network">our website</Link>!
-                            </p>
-                            <p>• Create / log on your PNID account and simply start Mario Kart 8</p>
-                        </Alert>
-                        <Alert variant="success">
-                            <Alert.Heading>Is there any rules?</Alert.Heading>
-                            <hr />
-                            <p>• You may only use cheats in friend rooms</p>
-                            <p>• You should not disrupt gameplay or server uptime</p>
-                            <p>• {"Do not generate any user-content (tournament, Mii names) with injury or NSFW content."}</p>
-                            <p>• You may have fun. {"(If red shells allow you)"}</p>
-                            <p>• Have common sense, we may issue a ban even for something that is not on list</p>
-                            <hr />
-                            <p className="mb-0">
-                                <strong>Breaching the rules can get your account or console temporarily/permanently banned from our services</strong>{" "}
-                                (🤓)
-                            </p>
-                        </Alert>
-                    </Tab>
-                    <Tab eventKey="features" title="Features">
-                        <h3>{"What's implemented ?"}</h3>
-
-                        <div className={`mx-auto w-auto`}>
-                            <ListGroup className="mb-3">
-                                <strong>
-                                    <ListGroup.Item variant="primary">Works for everyone</ListGroup.Item>
-                                    <ListGroup.Item variant="info">
-                                        Fully/partially a Miiverse related feature, might no be available on Cemu, might require a Tester account.
-                                    </ListGroup.Item>
-                                    <ListGroup.Item variant="danger">Does NOT work</ListGroup.Item>
-                                </strong>
-                            </ListGroup>
-                            <hr />
-
-                            <ListGroup className="mt-3 mb-3">
-                                <strong>
-                                    <ListGroup.Item variant="primary">Global matchmaking</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Regional matchmaking</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Friend rooms</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Join a friend by the friend list / friend presence</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Join a recent player</ListGroup.Item>
-                                </strong>
-                            </ListGroup>
-                            <hr />
-
-                            <ListGroup className="mt-3 mb-3">
-                                <strong>
-                                    <ListGroup.Item variant="info">Tournaments creation / deletion / update</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Tournament participation</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Tournament search by filter or code</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Tournament rankings (team and normal)</ListGroup.Item>
-                                    <ListGroup.Item variant="info">Post in the Tournament community</ListGroup.Item>
-                                    <ListGroup.Item variant="info">View posts from the tournament community</ListGroup.Item>
-                                </strong>
-                            </ListGroup>
-                            <hr />
-
-                            <ListGroup className="mt-3 mb-3">
-                                <strong>
-                                    <ListGroup.Item variant="primary">Rankings</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Rankings ghost download</ListGroup.Item>
-                                    <ListGroup.Item variant="info">Rankings Miiverse posts download</ListGroup.Item>
-                                    <ListGroup.Item variant="info">Rankings ghost upload</ListGroup.Item>
-                                </strong>
-                            </ListGroup>
-                            <hr />
-
-                            <ListGroup className="mt-3 mb-3">
-                                <strong>
-                                    <ListGroup.Item variant="primary">Mario Kart TV</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Mario Kart TV global / popular / recent highlights</ListGroup.Item>
-                                    <ListGroup.Item variant="info">Mario Kart TV highlight download</ListGroup.Item>
-                                    <ListGroup.Item variant="primary">Mario Kart TV highlight upload</ListGroup.Item>
-                                    <ListGroup.Item variant="danger">Mario Kart TV highlight upload on YouTube</ListGroup.Item>
-                                </strong>
-                            </ListGroup>
-                            <hr />
-                        </div>
-                    </Tab>
-                    <Tab eventKey="contact" title="CEMU">
-                        <Alert variant="info">
-                            <p>• Use CEMU 2.0-43 experimental or higher</p>
-                            <p>• {"We don't support piracy and we recommend dumping files from your own console."}</p>
-                            <p>• {"Files downloaded from the website aren't supported anymore"}</p>
-                            <p>
-                                •{" "}
-                                {
-                                    "If you have unstable frames, plesase don't go online, it ruins the experience for everyone, if it happens too much you will be banned."
-                                }
-                            </p>
-                        </Alert>
-                        <Alert>
-                            <p>
-                                The server is available on console and CEMU, follow the tutorial on{" "}
-                                <Link href="https://pretendo.network">the website</Link> to get Pretendo Network on your device.
-                            </p>
-                        </Alert>
-                    </Tab>
-                </Tabs>
-            </form>
-        </div>
-    );
-
-    /*
+	/*
 
 
     Mario Kart TV highlight automatic search (on mktv boot, global, popular/recent)
--- middleware.ts
+++ middleware.ts
@@ -1,107 +1,98 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { type JWTTokenPayload, getMK8Token, getMK8TokenFromAccountAPI } from './helpers/types/JWTTokenPayload';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { type JWTTokenPayload, getMK8Token, getMK8TokenFromAccountAPI } from "./helpers/types/JWTTokenPayload";
 
 export async function middleware(request: NextRequest) {
+	const nextPathname = request.nextUrl.pathname;
+	const allowedPIDs: number[] = [1606041002]; // PretendoRambo3
 
-    const nextPathname = request.nextUrl.pathname;
+	if (nextPathname.startsWith("/logout")) {
+		const url = new URL("/", request.url);
+		const response = NextResponse.redirect(url);
 
-    if (nextPathname.startsWith('/logout')) {
-        const url = new URL('/', request.url);
-        const response = NextResponse.redirect(url);
+		response.cookies.set("mk8_token", "", { maxAge: 0, domain: ".pretendo.network" });
+		response.cookies.set("access_token", "", { maxAge: 0, domain: ".pretendo.network" });
+		response.cookies.set("refresh_token", "", { maxAge: 0, domain: ".pretendo.network" });
+		response.cookies.set("token_type", "", { maxAge: 0, domain: ".pretendo.network" });
+		return response;
+	}
 
-        response.cookies.set("mk8_token", "", { maxAge: 0, domain: ".pretendo.network" });
-        response.cookies.set("access_token", "", { maxAge: 0, domain: ".pretendo.network" });
-        response.cookies.set("refresh_token", "", { maxAge: 0, domain: ".pretendo.network" });
-        response.cookies.set("token_type", "", { maxAge: 0, domain: ".pretendo.network" });
-        return response;
-    }
+	const hostname = request.nextUrl.hostname;
+	const redirect_login_url = `https://${hostname.substring(hostname.indexOf(".") + 1)}/account/login?redirect=http://${hostname}`;
 
-    const hostname = request.nextUrl.hostname;
-    const redirect_login_url = `https://${hostname.substring(hostname.indexOf('.') + 1)}/account/login?redirect=http://${hostname}`;
+	var mk8_token: JWTTokenPayload | null = await getMK8Token(request);
+	let res;
+	if (!mk8_token) {
+		res = await getMK8TokenFromAccountAPI(request);
+		if (!res && nextPathname.startsWith("/admin")) {
+			const response = NextResponse.redirect(redirect_login_url);
+			if (request.cookies.has("mk8_token")) {
+				response.cookies.delete("mk8_token");
+			}
+			return response;
+		}
+		if (res) {
+			mk8_token = res.token;
+		}
+	}
 
-    var mk8_token: JWTTokenPayload | null = await getMK8Token(request);
-    let res;
-    if (!mk8_token) {
-        res = await getMK8TokenFromAccountAPI(request);
-        if (!res && nextPathname.startsWith('/admin')) {
-            const response = NextResponse.redirect(redirect_login_url);
-            if (request.cookies.has("mk8_token")) {
-                response.cookies.delete("mk8_token");
-            }
-            return response;
-        }
-        if (res) {
-            mk8_token = res.token;
-        }
-    }
+	if (nextPathname.startsWith("/dashboard") && !mk8_token) {
+		const url = new URL("/", request.url);
+		return NextResponse.redirect(url);
+	}
 
-    if (nextPathname.startsWith('/dashboard') && !mk8_token) {
-        const url = new URL('/', request.url);
-        return NextResponse.redirect(url);
-    }
+	if (nextPathname.startsWith("/api/admin")) {
+		if (mk8_token) {
+			if (nextPathname.startsWith("/api/admin/userdata")) {
+				return NextResponse.next();
+			} else if (mk8_token.access_level >= 3) {
+				return NextResponse.next();
+			} else {
+				return new NextResponse("{}", { status: 401 });
+			}
+		} else {
+			return new NextResponse("{}", { status: 401 });
+		}
+	}
 
-    if (nextPathname.startsWith('/api/admin')) {
-        if (mk8_token) {
-            if (nextPathname.startsWith('/api/admin/userdata')) {
-                return NextResponse.next();
-            } else if (mk8_token.access_level >= 3) {
-                return NextResponse.next();
-            } else {
-                return new NextResponse("{}", { status: 401 });
-            }
-        } else {
-            return new NextResponse("{}", { status: 401 });
-        }
-    }
+	if (nextPathname.startsWith("/admin")) {
+		if (mk8_token) {
+			const isAdmin = mk8_token.access_level >= 3 && allowedPIDs.includes(mk8_token.pid);
+			if (!isAdmin) {
+				var response = NextResponse.redirect(new URL("/", request.url));
+			} else {
+				var response = NextResponse.next();
+			}
 
-    if (nextPathname.startsWith('/admin')) {
-        if (mk8_token) {
-            if (mk8_token.access_level < 3) {
-                var response = NextResponse.redirect(new URL('/', request.url));
-            } else {
-                var response = NextResponse.next();
-            }
-
-            response.headers.set("X-MK8-Pretendo-ACL", mk8_token.access_level.toString());
-            response.headers.set("X-MK8-Pretendo-Username", mk8_token.pnid);
-            response.headers.set("X-MK8-Pretendo-ImageURL", mk8_token.mii_image_url);
-            response.headers.set("X-MK8-Pretendo-PID", mk8_token.pid.toString());
-            if (res) {
-                response.cookies.set("mk8_token", res.jwt_token, { domain: ".pretendo.network" });
-            }
-            return response;
-        } else {
-            const response = NextResponse.redirect(redirect_login_url);
-            if (request.cookies.has("mk8_token")) {
-                response.cookies.delete("mk8_token");
-            }
-            return response;
-        }
-
-    } else {
-        const response = NextResponse.next();
-        if (mk8_token) {
-            response.headers.set("X-MK8-Pretendo-ACL", mk8_token.access_level.toString());
-            response.headers.set("X-MK8-Pretendo-Username", mk8_token.pnid);
-            response.headers.set("X-MK8-Pretendo-ImageURL", mk8_token.mii_image_url);
-            response.headers.set("X-MK8-Pretendo-PID", mk8_token.pid.toString());
-        }
-        if (res) {
-            response.cookies.set("mk8_token", res.jwt_token, { domain: ".pretendo.network" });
-        }
-        return response;
-    }
+			response.headers.set("X-MK8-Pretendo-ACL", mk8_token.access_level.toString());
+			response.headers.set("X-MK8-Pretendo-Username", mk8_token.pnid);
+			response.headers.set("X-MK8-Pretendo-ImageURL", mk8_token.mii_image_url);
+			response.headers.set("X-MK8-Pretendo-PID", mk8_token.pid.toString());
+			if (res) {
+				response.cookies.set("mk8_token", res.jwt_token, { domain: ".pretendo.network" });
+			}
+			return response;
+		} else {
+			const response = NextResponse.redirect(redirect_login_url);
+			if (request.cookies.has("mk8_token")) {
+				response.cookies.delete("mk8_token");
+			}
+			return response;
+		}
+	} else {
+		const response = NextResponse.next();
+		if (mk8_token) {
+			response.headers.set("X-MK8-Pretendo-ACL", mk8_token.access_level.toString());
+			response.headers.set("X-MK8-Pretendo-Username", mk8_token.pnid);
+			response.headers.set("X-MK8-Pretendo-ImageURL", mk8_token.mii_image_url);
+			response.headers.set("X-MK8-Pretendo-PID", mk8_token.pid.toString());
+		}
+		if (res) {
+			response.cookies.set("mk8_token", res.jwt_token, { domain: ".pretendo.network" });
+		}
+		return response;
+	}
 }
 export const config = {
-    matcher: [
-        '/',
-        '/logout',
-        '/api/:path*',
-        '/admin/:path*',
-        '/dashboard/:path*',
-        '/tournaments/:path*',
-        '/gatherings/:path*',
-        '/rankings/:path*'
-    ]
-}
+	matcher: ["/", "/logout", "/api/:path*", "/admin/:path*", "/dashboard/:path*", "/tournaments/:path*", "/gatherings/:path*", "/rankings/:path*"],
+};
