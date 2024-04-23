9617158184a8002d53a2c4813011157b2258badc
Christopher Angelo <angelo@angeloanan.xyz>
2024-04-23 23:44:40 +0700

    feat(tournamententry): memoize & tabular number

--- components/TournamentEntry/TournamentEntry.tsx
+++ components/TournamentEntry/TournamentEntry.tsx
@@ -1,10 +1,11 @@
-import './TournamentEntry.module.css'
+import styles from './TournamentEntry.module.css'
 import { Tournament } from '@/helpers/proto/amkj_service';
 import Image from 'next/image';
 import { Badge, Card, OverlayTrigger, Popover } from 'react-bootstrap';
 import { BsFillPersonFill, BsGlobe, BsSpeedometer2 } from 'react-icons/bs';
 import { AiFillShopping } from 'react-icons/ai';
 import { RiTeamFill } from 'react-icons/ri';
+import { memo } from 'react';
 
 const AMKJ_TOURNAMENT_ICONS: string[] = [
     'Ch_Mro', 'Ch_Lig', 'Ch_Pch', 'Ch_Dsy', 'Ch_Rst',
@@ -63,10 +64,6 @@ function getTournamentTimeFixed(value: Date): string {
     return value.toLocaleString();
 }
 
-export interface TournamentEntryProps {
-    tournament: Tournament;
-}
-
 const getTournamentTime = (tournament: Tournament): [string, string] => {
     switch (tournament.repeatType) {
         case 1: // Weekly
@@ -82,87 +79,93 @@ const getTournamentTime = (tournament: Tournament): [string, string] => {
     }
 }
 
-export const TournamentEntry: React.FC<TournamentEntryProps> = ({ tournament }) => {
-    const [startTime, endTime] = getTournamentTime(tournament);
-    
-    // Tournament Icon index may be negative or more than icons length. If so, default to `Ch_Mro` 
-    const tournamentIconUrl = `/assets/compe_icon/${AMKJ_TOURNAMENT_ICONS[tournament.iconType] ?? 'Ch_Mro'}.png`;
-
-    const isTournamentOfficial = (tournament.attributes[12] == 2);
-    
-    const raceMode = AMKJ_TOURNAMENT_RACE_MODE[tournament.attributes[2]] ?? "Invalid race mode"
-    const availableCourses = AMKJ_TOURNAMENT_AVAILABLE_COURSES[tournament.attributes[9]] ?? "Invalid courses"
-    const regionType = AMKJ_REGION_TYPE[tournament.attributes[7]] ?? "Invalid region"
-
-    const tournamentDescPopover = (
-        <Popover className="text-center">
-            <Popover.Header as="h3">Tournament details</Popover.Header>
-            <Popover.Body>
-                <Card>
-                    <Card.Header>
-                        <div className="d-flex align-items-center justify-content-center">
-                            <BsSpeedometer2 size={25} className="me-3 text-primary" />{raceMode}
-                        </div>
-                    </Card.Header>
-                    <Card.Header>
-                        <div className="d-flex align-items-center justify-content-center">
-                            <AiFillShopping size={25} className="me-3 text-primary" />{availableCourses}
-                        </div>
-                    </Card.Header>
-                    <Card.Header>
-                        <div className="d-flex align-items-center justify-content-center">
-                            <BsGlobe size={25} className="me-3 text-primary" />{regionType}
-                        </div>
-                    </Card.Header>
-                    <Card.Header>
-                        <div className="d-flex align-items-center justify-content-center">
-                            <RiTeamFill size={25} className="me-3 text-primary" />{
-                                tournament.attributes[4] == 2 ? (
-                                    <strong>
-                                        <span className="text-danger me-1">{tournament.redTeam}</span>vs<span className="text-primary ms-1">{tournament.blueTeam}</span>
-                                    </strong>
-                                ) : "No teams"
-                            }
-                        </div>
-                    </Card.Header>
-                </Card>
-            </Popover.Body>
-        </Popover>
-    );
-
-    return (
-        <OverlayTrigger trigger={["hover", "focus"]} placement="top" overlay={tournamentDescPopover}>
-            <Card className={`border ${isTournamentOfficial ? "border-warning" : "border-dark"}`}>
-                <Card.Header className="text-center">
-                    <div className="d-flex d-row justify-content-between">
-                        <strong>Tournament #{tournament.id}</strong>
-                        <strong>Season #{tournament.seasonId}</strong>
-                    </div>
-                </Card.Header>
-                <Card.Body>
-                    <div className="d-flex d-row justify-content-between">
-                        <div>
-                            <Image className="border" src={tournamentIconUrl} width={72} height={72} alt={`Tournament ${tournament.id}`} />
-                        </div>
-                        <div className="d-flex flex-column text-center align-self-center">
-
-                            {isTournamentOfficial && (
-                                <div className="align-items-center">
-                                    <Badge>Official</Badge>
-                                </div>
-                            )}
-                            <h4>{tournament.name}</h4>
-                            <h6>{tournament.description}</h6>
-                        </div>
-                        <div className="align-self-center">
-                            <BsFillPersonFill size={25} />{tournament.totalParticipants}
-                        </div>
-                    </div>
-                </Card.Body>
-                <Card.Footer className="text-center">Start: <strong>{startTime}</strong></Card.Footer>
-                <Card.Footer className="text-center">End: <strong>{endTime}</strong></Card.Footer>
-                <Card.Footer className="text-center">Community code: <strong>{tournament.communityCode.match(/\d{4}/g)?.join('-')}</strong></Card.Footer>
-            </Card>
-        </OverlayTrigger>
-    )
+export interface TournamentEntryProps {
+    tournament: Tournament;
 }
+
+export const TournamentEntry: React.FC<TournamentEntryProps> = memo(
+    function TournamentEntry({ tournament }) {
+        const [startTime, endTime] = getTournamentTime(tournament);
+        
+        // Tournament Icon index may be negative or more than icons length. If so, default to `Ch_Mro` 
+        const tournamentIconUrl = `/assets/compe_icon/${AMKJ_TOURNAMENT_ICONS[tournament.iconType] ?? 'Ch_Mro'}.png`;
+
+        const isTournamentOfficial = (tournament.attributes[12] == 2);
+        
+        const raceMode = AMKJ_TOURNAMENT_RACE_MODE[tournament.attributes[2]] ?? "Invalid race mode"
+        const availableCourses = AMKJ_TOURNAMENT_AVAILABLE_COURSES[tournament.attributes[9]] ?? "Invalid courses"
+        const regionType = AMKJ_REGION_TYPE[tournament.attributes[7]] ?? "Invalid region"
+
+        const tournamentDescPopover = (
+            <Popover className="text-center">
+                <Popover.Header as="h3">Tournament details</Popover.Header>
+                <Popover.Body>
+                    <Card>
+                        <Card.Header>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <BsSpeedometer2 size={25} className="me-3 text-primary" />{raceMode}
+                            </div>
+                        </Card.Header>
+                        <Card.Header>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <AiFillShopping size={25} className="me-3 text-primary" />{availableCourses}
+                            </div>
+                        </Card.Header>
+                        <Card.Header>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <BsGlobe size={25} className="me-3 text-primary" />{regionType}
+                            </div>
+                        </Card.Header>
+                        <Card.Header>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <RiTeamFill size={25} className="me-3 text-primary" />{
+                                    tournament.attributes[4] == 2 ? (
+                                        <strong>
+                                            <span className="text-danger me-1">{tournament.redTeam}</span>vs<span className="text-primary ms-1">{tournament.blueTeam}</span>
+                                        </strong>
+                                    ) : "No teams"
+                                }
+                            </div>
+                        </Card.Header>
+                    </Card>
+                </Popover.Body>
+            </Popover>
+        );
+
+        return (
+            <OverlayTrigger trigger={["hover", "focus"]} placement="top" overlay={tournamentDescPopover}>
+                <Card className={`border ${isTournamentOfficial ? "border-warning" : "border-dark"}`}>
+                    <Card.Header className="text-center">
+                        <div className="d-flex d-row justify-content-between">
+                            <span className={`fw-bold ${styles.tabular}`}>#{tournament.id}</span>
+                            <span className='fw-bold'>Season {tournament.seasonId}</span>
+                        </div>
+                    </Card.Header>
+                    <Card.Body>
+                        <div className="d-flex d-row justify-content-between gap-2 ">
+                            <div>
+                                <Image className="border" src={tournamentIconUrl} width={72} height={72} alt={`Tournament ${tournament.id}`} />
+                            </div>
+                            <div className="d-flex flex-column text-center align-self-center">
+
+                                {isTournamentOfficial && (
+                                    <div className="align-items-center">
+                                        <Badge>Official</Badge>
+                                    </div>
+                                )}
+                                <h4>{tournament.name}</h4>
+                                <h6>{tournament.description}</h6>
+                            </div>
+                            <div className="align-self-center">
+                                <BsFillPersonFill size={25} />{tournament.totalParticipants}
+                            </div>
+                        </div>
+                    </Card.Body>
+                    <Card.Footer className="text-center">Start: <strong>{startTime}</strong></Card.Footer>
+                    <Card.Footer className="text-center">End: <strong>{endTime}</strong></Card.Footer>
+                    <Card.Footer className="text-center">Community code: <strong>{tournament.communityCode.match(/\d{4}/g)?.join('-')}</strong></Card.Footer>
+                </Card>
+            </OverlayTrigger>
+        )
+    }
+)
