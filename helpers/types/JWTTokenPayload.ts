8335b1686cedd90475ae2d3ec8280b64d5ca806d
mrjvs <jellevs@gmail.com>
2026-09-06 14:29:09 +0200

    feat: update JWT lib + strict enforce algorithms

--- helpers/types/JWTTokenPayload.ts
+++ helpers/types/JWTTokenPayload.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from 'next/server'
 import app_config from '@/app.config';
-import { SignJWT, jwtVerify } from 'jose';
+import { JWSAlgorithm, SignJWT, jwtVerify } from 'jose';
 import { nanoid } from 'nanoid';
 import { legacyApiGrpcClient } from '../grpc';
 import { Metadata } from 'nice-grpc';
@@ -12,6 +12,9 @@ export type JWTTokenPayload = {
     pid: number;
 }
 
+const alg: JWSAlgorithm = 'HS256';
+const jwtSecret = new TextEncoder().encode(app_config.jwt_secret);
+
 export async function getMK8Token(request: NextRequest): Promise<JWTTokenPayload | null> {
     const mk8_token = request.cookies.get("mk8_token")?.value;
     if (!mk8_token) {
@@ -19,8 +22,10 @@ export async function getMK8Token(request: NextRequest): Promise<JWTTokenPayload
     }
 
     try {
-        const verifyResult = await jwtVerify(mk8_token, new TextEncoder().encode(app_config.jwt_secret));
-        return verifyResult.payload as JWTTokenPayload;
+        const verifyResult = await jwtVerify<JWTTokenPayload>(mk8_token, jwtSecret, {
+            algorithms: [alg]
+        });
+        return verifyResult.payload;
     } catch (error) { }
 
     return null;
@@ -28,8 +33,10 @@ export async function getMK8Token(request: NextRequest): Promise<JWTTokenPayload
 
 export async function getMK8TokenEx(mk8_token: string): Promise<JWTTokenPayload | null> {
     try {
-        const verifyResult = await jwtVerify(mk8_token, new TextEncoder().encode(app_config.jwt_secret));
-        return verifyResult.payload as JWTTokenPayload;
+        const verifyResult = await jwtVerify<JWTTokenPayload>(mk8_token, jwtSecret, {
+            algorithms: [alg],
+        });
+        return verifyResult.payload;
     } catch (error) { }
 
     return null;
@@ -56,11 +63,11 @@ export async function getMK8TokenFromAccountAPI(request: NextRequest): Promise<{
         }
 
         const token = await new SignJWT(token_data)
-            .setProtectedHeader({ alg: 'HS256' })
+            .setProtectedHeader({ alg })
             .setJti(nanoid())
             .setIssuedAt()
             .setExpirationTime('2h')
-            .sign(new TextEncoder().encode(app_config.jwt_secret));
+            .sign(jwtSecret);
 
         return { token: token_data, jwt_token: token };
     } catch (error) { }
